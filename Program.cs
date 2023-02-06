e92e5834341da12318b319cfcd409740eae9f0c1
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-02-06 22:02:21 +0530

    Added to remote

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAssignment
+{    
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Hashtable Program");
+                Linked_Hash_Map<string, int> linked_hash_map = new Linked_Hash_Map<string, int>(5);
+
+                string Sentence = "paranoids are not paranoid because they are paranoid but because " +
+                    "they keep putting themselves deliberately into paranoid avoidable situations";
+                string[] Words = Sentence.Split(" ");
+                foreach (string word in Words)
+                {
+                    int Value = linked_hash_map.Get(word);
+                    if (Value == default)
+                        Value = 1;
+                    else Value++;
+                    linked_hash_map.Add(word, Value);
+                }
+                int Frequency = linked_hash_map.Get("paranoid");
+                Console.WriteLine("\"paranoid\" comes {0} times in the given paragraph", Frequency);
+                Frequency = linked_hash_map.Get("are");
+                Console.WriteLine("\"are\" comes {0} times in the given paragraph", Frequency);
+                Frequency = linked_hash_map.Get("they");
+                Console.WriteLine("\"they\" comes {0} times in the given paragraph", Frequency);
+            }
+        }
+  
+}
